2f879e201646be77141c1b53936e896778a64732
Meow-Chu <chuyeon0314@gmail.com>
2024-01-29 23:40:42 +0900

    MERN Stack Social Media App Tutorial

---
+++ ZAINKEEPSCODE/SocialMedia-Starter/src/components/profileSide/ProfileSide.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import LogoSearch from "../LogoSearch/LogoSearch";
+import "./ProfileSide.css";
+
+export default function ProfileSide() {
+  return (
+    <div className="ProfileSide">
+      <LogoSearch />
+    </div>
+  );
+}
