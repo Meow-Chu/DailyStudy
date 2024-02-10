c55c69d1e14a7cc8474cd589e6f6215dd6b47b86
Meow-Chu <chuyeon0314@gmail.com>
2024-02-10 16:42:38 +0900

    Delete a Post

--- ZAINKEEPSCODE/server/Controllers/PostController.js
+++ ZAINKEEPSCODE/server/Controllers/PostController.js
@@ -43,6 +43,22 @@ export const updatePost = async (req, res) => {
   }
 };
 
+// Delete a Post
+export const deletePost = async (req, res) => {
+  const id = req.params.id;
+  const { userId } = req.body;
+
+  try {
+    const post = await PostModel.findById(id);
+    if (post.userId === userId) {
+      await post.deleteOne();
+      res.status(200).json("Post Deleted successfully");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
 /**
  * (1) const newPost = req.body로 시작하는 것보다 new PostModel함수를 사용하는 이유는.. req의 body를 받자마자 그것을 우리의 post에 삽입하게 된다. 그런 후 newPost에 우리의 post model의 객체를 담게 된다. new PostModel이라는 2 단어를 추가함으로써 추가로 작성할 코드량을 줄여줄 수 있음
  * (2) Update a Post
