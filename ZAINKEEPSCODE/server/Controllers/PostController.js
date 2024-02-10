d1ea6a4cbc419c1ec8e8f2e165b0a0a9291e44a2
Meow-Chu <chuyeon0314@gmail.com>
2024-02-10 16:08:10 +0900

    Get a Post

--- ZAINKEEPSCODE/server/Controllers/PostController.js
+++ ZAINKEEPSCODE/server/Controllers/PostController.js
@@ -13,6 +13,18 @@ export const createPost = async (req, res) => {
   }
 };
 
+// Get a post
+export const getPost = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const post = await PostModel.findById(id);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
 /**
  * (1) const newPost = req.body로 시작하는 것보다 new PostModel함수를 사용하는 이유는.. req의 body를 받자마자 그것을 우리의 post에 삽입하게 된다. 그런 후 newPost에 우리의 post model의 객체를 담게 된다. new PostModel이라는 2 단어를 추가함으로써 추가로 작성할 코드량을 줄여줄 수 있음
  *
